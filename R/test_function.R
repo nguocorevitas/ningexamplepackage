83a192d8363d41780c9a2fc393a1ff3bf9afeda5
Xiaoning.Guo <xiaoning.guo@thermofisher.com>
2026-03-13 11:21:09 -0400

    update

--- R/test_function.R
+++ R/test_function.R
@@ -3,11 +3,11 @@
 #' @param num1 First number
 #' @param num2 Second number
 #'
-#' @returns A sum of the 2 numbers
+#' @returns A sum of the 2 numbers with square
 #' @export
 
 test_function <- function(num1, num2){
-  result = num1 + num2
+  result = num1 + num2**2
 
   return(result)
 }
