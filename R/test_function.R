c51b4f56d5c6b6cda26097fd539da04238a6c6a4
Xiaoning.Guo <xiaoning.guo@thermofisher.com>
2026-03-13 11:11:21 -0400

    Added first function

---
+++ R/test_function.R
@@ -0,0 +1,13 @@
+#' Title: Test function for educational purpose
+#'
+#' @param num1 First number
+#' @param num2 Second number
+#'
+#' @returns A sum of the 2 numbers
+#' @export
+
+test_function <- function(num1, num2){
+  result = num1 + num2
+
+  return(result)
+}
